a3c1a175d5a81d4ec39a8c1b6ea3d615126cfaf3
RedHeadMatt <m.k.franczak@gmail.com>
2018-08-25 16:45:03 +0200

    first commit

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/com/kodilla/testing/library/BookLibrary.java
@@ -0,0 +1,36 @@
+package com.kodilla.testing.com.kodilla.testing.library;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookLibrary {
+
+    LibraryDatabase libraryDatabase;
+
+    public BookLibrary(LibraryDatabase libraryDatabase) {
+        this.libraryDatabase = libraryDatabase;
+
+    }
+
+    public List<Book> listBooksWithCondition(String titleFragment) {
+        List<Book> bookList = new ArrayList<Book>();
+        if (titleFragment.length() < 3) return bookList;
+        List<Book> resultList = libraryDatabase
+                .listBooksWithCondition(titleFragment);
+        if (resultList.size() > 20) return bookList;
+        bookList = resultList;
+        return bookList;
+    }
+
+    public int listBookInHandsOf(LibraryUser libraryUser){
+
+        List<Book> theList = new ArrayList<Book>();
+
+        theList = libraryDatabase.listBooksInHandsOf(libraryUser);
+
+        return theList.size();
+
+
+
+    }
+}
