752c9bf3a7ea4e77d843191d2fce79119b17f576
RedHeadMatt <m.k.franczak@gmail.com>
2018-08-26 14:23:00 +0200

    first commit

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,7 +1,14 @@
 package com.kodilla.stream;
 
+
+
+
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.*;
+
+import static com.kodilla.stream.iterate.NumbersGenerator.generateEven;
+
 
 public class StreamMain {
 
@@ -14,5 +21,10 @@ public class StreamMain {
         poemBeautifier.beautify("ASDASD", text -> "HHH"+text+"HHH");
         poemBeautifier.beautify("POSZLA OLA DO PRZEDSZKOLA", text -> text.replace("O", "!").toLowerCase().trim());
         poemBeautifier.beautify("TO JEST TEKST", text -> text = null);
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
+
+
     }
 }
